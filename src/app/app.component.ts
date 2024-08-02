171da1c306560a7780bf898555fcfba3e0bfe7c4
perceptron8 <perceptron8@users.noreply.github.com>
2024-08-02 18:37:30 +0200

    overlay?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { DropdownModule } from './dropdown';
+import { OverlayModule } from 'primeng/overlay';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [ DropdownModule ],
+	imports: [ OverlayModule ],
 	templateUrl: './app.component.html'
 })
 export class AppComponent {
